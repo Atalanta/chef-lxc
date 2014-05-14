d65e4dacc7c3be4e8df59b7c97dae9fa8b86fd38
Stephen <stephen@0214-pc24.pcfg.co.uk>
2014-05-14 04:01:45 +0100

    Make bridge details come from databag

--- recipes/bridge.rb
+++ recipes/bridge.rb
@@ -1,15 +1,35 @@
 include_recipe "network"
 include_recipe "network::bridge"
 
-network_config "br0" do
-  dhcp true
-  onboot true
-  nm_controlled false
-  bridge true 
-  action :create
+host_data = data_bag_item("lxc", "hostss")
+
+if node['lxc']['bridge'] = 'Automatic'
+
+  network_config "br0" do
+    dhcp true
+    onboot true
+    nm_controlled false
+    bridge true 
+    action :create
+  end
+  
+else
+  
+  network_config "br0" do
+    dhcp false
+    ipaddr host_data[node['hostname']]['ipaddress']
+    network host_data[node['hostname']]['network']
+    gateway host_data[node['hostname']]['gateway']
+    broadcast host_data[node['hostname']]['broadcast']     
+    onboot true
+    nm_controlled false
+    bridge true 
+    action :create
+  end
+
 end
 
-network_config "eth0" do
+network_config host_data[node['hostname']['nic']] do
   onboot true
   bridge "br0"
   nm_controlled false
