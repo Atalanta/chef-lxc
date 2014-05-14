f1de18812dd00a4b9c4e71a8acf7bbd1bf0164bd
Stephen <stephen@0214-pc24.pcfg.co.uk>
2014-05-14 03:28:11 +0100

    Include libs

--- recipes/default.rb
+++ recipes/default.rb
@@ -21,9 +21,19 @@ end
 package "libcap-devel"
 package "screen"
 
+remote_file '/tmp/lxc-libs-1.0.3-1.el6.x86_64.rpm' do
+  source 'https://s3-eu-west-1.amazonaws.com/c6lxc/lxc-libs-1.0.3-1.el6.x86_64.rpm'
+  notifies :run, 'execute[Install LXC Libs]'
+end
+
 remote_file '/tmp/lxc-1.0.3-1.el6.x86_64.rpm' do
   source 'https://s3-eu-west-1.amazonaws.com/c6lxc/lxc-1.0.3-1.el6.x86_64.rpm'
   notifies :run, 'execute[Install LXC]'
+end
+
+execute 'Install LXC Libs' do
+  command 'yum localinstall /tmp/lxc-libs-1.0.3-1.el6.x86_64.rpm'
+  action :nothing
 end
 
 execute 'Install LXC' do
